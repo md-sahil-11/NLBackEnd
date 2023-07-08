fd50bcc4de9409766103cadaec2dbaaab87a2328
md-sahil-11 <mdsahilalam11@gmail.com>
2023-07-09 01:45:32 +0530

    minor

--- core/settings/production.py
+++ core/settings/production.py
@@ -16,7 +16,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
 ]
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ORIGIN_WHITELIST = (
     "https://nl-frontend-seven.vercel.app",

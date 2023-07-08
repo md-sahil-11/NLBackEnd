f5fb24fd54c9deb6e58420cef84ef49e626b5b74
md-sahil-11 <mdsahilalam11@gmail.com>
2023-07-09 01:09:14 +0530

    minor

--- core/settings/base.py
+++ core/settings/base.py
@@ -27,9 +27,6 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
